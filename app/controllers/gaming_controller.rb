280a3ed48c7e34822f75055f9281e1ddeab8ad18
Edward Zhou <edward.zhq@gmail.com>
2012-05-03 18:51:23 +0800

    add OddsLevel

--- app/controllers/gaming_controller.rb
+++ app/controllers/gaming_controller.rb
@@ -15,13 +15,23 @@ class GamingController < ApplicationController
       ball_id = ball_regex.match(id)[1].to_i
     end
 
-    if ball_id == 0
-      render :action => "index"
-    elsif ball_id
-      @ball = @game.balls[ball_id - 1]
-    end
+    respond_to do |format|
+      format.html do
+        if ball_id == 0
+          render :action => "index"
+        elsif ball_id
+          @ball = @game.balls[ball_id - 1]
+        end
+      end
 
+      format.js do
+        @odds_level.to_json
+      end
+
+
+    end
   end
+
 
   private
   def init
@@ -36,11 +46,97 @@ class GamingController < ApplicationController
                               :third => 2.39, :c2 => 16, :p2 => 16, :c3 => 26,
                               :p3 => 26, :c4 => 56, :p4 => 56, :c5 => 190, :p5 => 190, :return => 0.010}
 
+    level_a = OddsLevel.new( {:level_name => "A盘", :level_description => "A盘描述", :return => 0.005} )
+    level_a.rules << Rule.new({:rule_name => :exact, :rule_title => "单球赔率", :rule_description => "单球赔率",
+                               :odds => 19.6, :sort_index => 1})
+    level_a.rules << Rule.new({:rule_name => :half, :rule_title => "两面赔率", :rule_description => "单双、大小、龙虎赔率",
+                               :odds => 1.984, :sort_index => 20})
+    level_a.rules << Rule.new({:rule_name => :third, :rule_title => "三面赔率", :rule_description => "中发白赔率",
+                               :odds => 2.79, :sort_index => 30})
+    level_a.rules << Rule.new({:rule_name => :quarter, :rule_title => "四面赔率", :rule_description => "东南西北赔率",
+                               :odds => 3.92, :sort_index => 40})
+    level_a.rules << Rule.new({:rule_name => :c2, :rule_title => "任选二赔率", :rule_description => "任选二赔率",
+                               :odds => 20, :sort_index => 50})
+    level_a.rules << Rule.new({:rule_name => :p2, :rule_title => "直选二赔率", :rule_description => "直选二赔率",
+                               :odds => 20, :sort_index => 60})
+    level_a.rules << Rule.new({:rule_name => :c3, :rule_title => "任选三赔率", :rule_description => "任选三赔率",
+                               :odds => 30, :sort_index => 70})
+    level_a.rules << Rule.new({:rule_name => :p3, :rule_title => "直选三赔率", :rule_description => "直选三赔率",
+                               :odds => 30, :sort_index => 80})
+    level_a.rules << Rule.new({:rule_name => :c4, :rule_title => "任选四赔率", :rule_description => "任选四赔率",
+                               :odds => 60, :sort_index => 90})
+    level_a.rules << Rule.new({:rule_name => :p4, :rule_title => "直选四赔率", :rule_description => "直选四赔率",
+                               :odds => 600, :sort_index => 100})
+    level_a.rules << Rule.new({:rule_name => :c5, :rule_title => "任选五赔率", :rule_description => "任选五赔率",
+                               :odds => 200, :sort_index => 110})
+    level_a.rules << Rule.new({:rule_name => :p5, :rule_title => "直选五赔率", :rule_description => "直选五赔率",
+                               :odds => 200, :sort_index => 120})
+
+
+    level_b = OddsLevel.new( {:level_name => "B盘", :level_description => "B盘描述", :return => 0.008} )
+    level_b.rules << Rule.new({:rule_name => :exact, :rule_title => "单球赔率", :rule_description => "单球赔率",
+                               :odds => 19.3, :sort_index => 1})
+    level_b.rules << Rule.new({:rule_name => :half, :rule_title => "两面赔率", :rule_description => "单双、大小、龙虎赔率",
+                               :odds => 1.93, :sort_index => 20})
+    level_b.rules << Rule.new({:rule_name => :third, :rule_title => "三面赔率", :rule_description => "中发白赔率",
+                               :odds => 2.59, :sort_index => 30})
+    level_b.rules << Rule.new({:rule_name => :quarter, :rule_title => "四面赔率", :rule_description => "东南西北赔率",
+                               :odds => 3.82, :sort_index => 40})
+    level_b.rules << Rule.new({:rule_name => :c2, :rule_title => "任选二赔率", :rule_description => "任选二赔率",
+                               :odds => 18, :sort_index => 50})
+    level_b.rules << Rule.new({:rule_name => :p2, :rule_title => "直选二赔率", :rule_description => "直选二赔率",
+                               :odds => 18, :sort_index => 60})
+    level_b.rules << Rule.new({:rule_name => :c3, :rule_title => "任选三赔率", :rule_description => "任选三赔率",
+                               :odds => 28, :sort_index => 70})
+    level_b.rules << Rule.new({:rule_name => :p3, :rule_title => "直选三赔率", :rule_description => "直选三赔率",
+                               :odds => 28, :sort_index => 80})
+    level_b.rules << Rule.new({:rule_name => :c4, :rule_title => "任选四赔率", :rule_description => "任选四赔率",
+                               :odds => 58, :sort_index => 90})
+    level_b.rules << Rule.new({:rule_name => :p4, :rule_title => "直选四赔率", :rule_description => "直选四赔率",
+                               :odds => 58, :sort_index => 100})
+    level_b.rules << Rule.new({:rule_name => :c5, :rule_title => "任选五赔率", :rule_description => "任选五赔率",
+                               :odds => 198, :sort_index => 110})
+    level_b.rules << Rule.new({:rule_name => :p5, :rule_title => "直选五赔率", :rule_description => "直选五赔率",
+                               :odds => 198, :sort_index => 120})
+
+    level_c = OddsLevel.new( {:level_name => "C盘", :level_description => "C盘描述", :return => 0.010} )
+    level_c.rules << Rule.new({:rule_name => :exact, :rule_title => "单球赔率", :rule_description => "单球赔率",
+                               :odds => 19, :sort_index => 1})
+    level_c.rules << Rule.new({:rule_name => :half, :rule_title => "两面赔率", :rule_description => "单双、大小、龙虎赔率",
+                               :odds => 1.9, :sort_index => 20})
+    level_c.rules << Rule.new({:rule_name => :third, :rule_title => "三面赔率", :rule_description => "中发白赔率",
+                               :odds => 2.39, :sort_index => 30})
+    level_c.rules << Rule.new({:rule_name => :quarter, :rule_title => "四面赔率", :rule_description => "东南西北赔率",
+                               :odds => 3.72, :sort_index => 40})
+    level_c.rules << Rule.new({:rule_name => :c2, :rule_title => "任选二赔率", :rule_description => "任选二赔率",
+                               :odds => 16, :sort_index => 50})
+    level_c.rules << Rule.new({:rule_name => :p2, :rule_title => "直选二赔率", :rule_description => "直选二赔率",
+                               :odds => 16, :sort_index => 60})
+    level_c.rules << Rule.new({:rule_name => :c3, :rule_title => "任选三赔率", :rule_description => "任选三赔率",
+                               :odds => 26, :sort_index => 70})
+    level_c.rules << Rule.new({:rule_name => :p3, :rule_title => "直选三赔率", :rule_description => "直选三赔率",
+                               :odds => 26, :sort_index => 80})
+    level_c.rules << Rule.new({:rule_name => :c4, :rule_title => "任选四赔率", :rule_description => "任选四赔率",
+                               :odds => 56, :sort_index => 90})
+    level_c.rules << Rule.new({:rule_name => :p4, :rule_title => "直选四赔率", :rule_description => "直选四赔率",
+                               :odds => 56, :sort_index => 100})
+    level_c.rules << Rule.new({:rule_name => :c5, :rule_title => "任选五赔率", :rule_description => "任选五赔率",
+                               :odds => 196, :sort_index => 110})
+    level_c.rules << Rule.new({:rule_name => :p5, :rule_title => "直选五赔率", :rule_description => "直选五赔率",
+                               :odds => 196, :sort_index => 120})
+
+
+
+    @odds_levels = {:LEVEL_A => level_a, :LEVEL_B => level_b, :LEVEL_C => level_c}
+
     if not params[:level].blank?
-      @level = params[:level].to_s.to_sym
+      @level = params[:level].to_s.upcase.to_sym
     end
 
     @level ||= :LEVEL_B
+
+    @odds_level = @odds_levels[@level]
+
 
     @sum_bet_odd_even_rules = []
     @sum_bet_odd_even_rules << Rule.new({:rule_name => "sum_odd", :rule_title => "總和單", :odds => "1.984"})
---
+++ app/models/odds_level.rb
@@ -0,0 +1,18 @@
+class OddsLevel
+  include Mongoid::Document
+  field :level_name, type: String
+  field :level_description, type: String
+  field :return, type: BigDecimal
+
+  embeds_many :rules
+
+  def rule_by_name(rule_name)
+    if @rule_hash.nil?
+      @rule_hash = {}
+      self.rules.each {|rule| @rule_hash[rule.rule_name.to_sym] = rule}
+    end
+
+    @rule_hash[rule_name.to_sym]
+  end
+
+end
