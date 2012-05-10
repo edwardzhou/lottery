5aaeb9a70d4726b162ed4374e9ff282a669f60e7
Edward Zhou <edward@edward-OptiPlex-390.(none)>
2012-05-10 18:34:59 +0800

    remote update

--- app/controllers/admin/odds_levels_controller.rb
+++ app/controllers/admin/odds_levels_controller.rb
@@ -15,13 +15,17 @@ class Admin::OddsLevelsController < ApplicationController
     @odds_level.rules.each do |rule|
       logger.debug("before update, rule[id=> #{rule.id}, name=> #{rule.rule_name}] odds => #{rule.odds}")
       rule.odds = params["rule_#{rule.id}".to_sym][:odds]
+      rule.active = params["rule_#{rule.id}".to_sym][:active]
       logger.debug("after update, rule[id=> #{rule.id}, name=> #{rule.rule_name}] odds => #{rule.odds}")
     end
 
     #@odds_level.save!
     @lottery_def.save!
 
-    redirect_to edit_admin_lottery_def_odds_level_path(@lottery_def, @odds_level)
+    respond_to do |format|
+      format.html {redirect_to edit_admin_lottery_def_odds_level_path(@lottery_def, @odds_level)}
+    end
+
   end
 
 
