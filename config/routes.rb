48eaa53a0a5778ef957a9afcbf9a3f080d5e9dd7
Edward Zhou <edward.zhq@gmail.com>
2012-05-13 23:35:43 +0800

    config root

--- config/routes.rb
+++ config/routes.rb
@@ -70,7 +70,7 @@ Lottery::Application.routes.draw do
   #   end
 
   # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
+  # just remember to delete public/index2.html.
   # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
