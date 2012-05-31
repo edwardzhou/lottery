9914968c22fbcf967c9c982596413009abfd84d9
Edward Zhou <edward.zhq@gmail.com>
2012-05-31 09:07:19 +0800

    move methods into module for reuse

--- app/models/lottery_inst.rb
+++ app/models/lottery_inst.rb
@@ -11,42 +11,12 @@ class LotteryInst
   field :end_time, type: Time
   field :close_at, type: Time
   field :period, type: Integer
-  field :active, type: Boolean
+  field :accept_betting, type: Boolean, default: false
+  field :active, type: Boolean, default: false
   field :return_rate, type: Integer
   field :total_income, type: BigDecimal
   field :total_outcome, type: BigDecimal
   field :profit, type: BigDecimal
-
-=begin
-  field :ball_1, type: Integer
-  field :ball_1_trail, type: Integer #1号球尾数
-  field :ball_1_add, type: Integer #1号球合数
-  field :ball_2, type: Integer
-  field :ball_2_trail, type: Integer #2号球尾数
-  field :ball_2_add, type: Integer #2号球合数
-  field :ball_3, type: Integer
-  field :ball_3_trail, type: Integer #3号球尾数
-  field :ball_3_add, type: Integer #3号球合数
-  field :ball_4, type: Integer
-  field :ball_4_trail, type: Integer #4号球尾数
-  field :ball_4_add, type: Integer #4号球合数
-  field :ball_5, type: Integer
-  field :ball_5_trail, type: Integer #5号球尾数
-  field :ball_5_add, type: Integer #5号球合数
-  field :ball_6, type: Integer
-  field :ball_6_trail, type: Integer #6号球尾数
-  field :ball_6_add, type: Integer #6号球合数
-  field :ball_7, type: Integer
-  field :ball_7_trail, type: Integer #7号球尾数
-  field :ball_7_add, type: Integer #7号球合数
-  field :ball_8, type: Integer
-  field :ball_8_trail, type: Integer #8号球尾数
-  field :ball_8_add, type: Integer #8号球合数
-  field :ball_9, type: Integer #总和
-  field :ball_9_trail, type: Integer #总和尾数
-  field :ball_9_add, type: Integer #总和合数
-  field :ball_10, type: Integer #龙虎 (1 龙， 2 虎)
-=end
 
   embeds_one :ball_1, :class_name => "Ball"
   embeds_one :ball_2, :class_name => "Ball"
@@ -67,85 +37,6 @@ class LotteryInst
 
   include Mongoid::Timestamps
 
-  def get_odds_level(level_id)
-    if @odds_level_map.nil?
-      @odds_level_map = {}
-      odds_levels.each {|l| @odds_level_map[l.level_id] = l}
-    end
-
-    @odds_level_map[level_id]
-  end
-
-  def bet_rule(rule_id)
-    rule_id = rule_id.to_sym unless rule_id.kind_of?(Symbol)
-    self.bet_rules.where(rule_id: rule_id).first
-  end
-
-  def set_ball_values(balls_values)
-
-    if self.ball_1.nil?
-      self.ball_1 = Ball.new
-    end
-    if self.ball_2.nil?
-      self.ball_2 = Ball.new
-    end
-    if self.ball_3.nil?
-      self.ball_3 = Ball.new
-    end
-    if self.ball_4.nil?
-      self.ball_4 = Ball.new
-    end
-    if self.ball_5.nil?
-      self.ball_5 = Ball.new
-    end
-    if self.ball_6.nil?
-      self.ball_6 = Ball.new
-    end
-    if self.ball_7.nil?
-      self.ball_7 = Ball.new
-    end
-    if self.ball_8.nil?
-      self.ball_8 = Ball.new
-    end
-    if self.ball_9.nil?
-      self.ball_9 = Ball.new
-    end
-    if self.ball_10.nil?
-      self.ball_10 = Ball.new
-    end
-
-    ball_sum = 0
-
-    if (balls_values.kind_of?(Hash))
-      self.ball_1.set_value(balls_values[:ball_1])
-      self.ball_2.set_value(balls_values[:ball_2])
-      self.ball_3.set_value(balls_values[:ball_3])
-      self.ball_4.set_value(balls_values[:ball_4])
-      self.ball_5.set_value(balls_values[:ball_5])
-      self.ball_6.set_value(balls_values[:ball_6])
-      self.ball_7.set_value(balls_values[:ball_7])
-      self.ball_8.set_value(balls_values[:ball_8])
-      ball_sum = balls_values.inject(0) {|sum, ball| sum = sum + ball[1] }
-    else
-      self.ball_1.set_value(balls_values[0])
-      self.ball_2.set_value(balls_values[1])
-      self.ball_3.set_value(balls_values[2])
-      self.ball_4.set_value(balls_values[3])
-      self.ball_5.set_value(balls_values[4])
-      self.ball_6.set_value(balls_values[5])
-      self.ball_7.set_value(balls_values[6])
-      self.ball_8.set_value(balls_values[7])
-      ball_sum = balls_values.inject(0) {|sum, ball| sum = sum + ball}
-    end
-
-    self.ball_9.set_value(ball_sum)
-
-    self.ball_10.dragon = self.ball_1.ball_value < self.ball_8.ball_value
-    self.ball_10.tiger = !self.ball_10.dragon
-
-
-
-  end
-
+  include LotteryMembers
 
 end
