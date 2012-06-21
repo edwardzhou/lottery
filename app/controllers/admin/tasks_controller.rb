38c9c2a3a988894c1cddc4a8b4aee1652cf949e6
Edward Zhou <edward.zhq@gmail.com>
2012-06-21 15:02:45 +0800

    change to verify token instead of IP

--- app/controllers/admin/tasks_controller.rb
+++ app/controllers/admin/tasks_controller.rb
@@ -1,11 +1,12 @@
 require "lottery_task"
 class Admin::TasksController < ApplicationController
   skip_before_filter :login_required
-  before_filter :check_local_ip
+  #before_filter :check_local_ip
+  before_filter :check_token
 
   def reset_av_credit
     LotteryTask.reset_available_credits
-    render :text => "reset ok. #{Time.now}"
+    render :text => "reset ok. #{Time.now}\n"
   end
 
   private
@@ -13,6 +14,13 @@ class Admin::TasksController < ApplicationController
     if request.remote_ip != "127.0.0.1"
       logger.warn("wrong task invoke from #{request.remote_ip}. redirect to home page")
       redirect_to root_path
+    end
+  end
+
+  def check_token
+    if params[:token] != "iHjrC7yIF8Bvsjck2J5QzmvgCjoiiiKJ"
+      logger.warn("wrong task invoke from #{params[:token]}. ")
+      render :text => "wrong task invoke from #{request.remote_ip}. #{Time.now}\n"
     end
   end
 
